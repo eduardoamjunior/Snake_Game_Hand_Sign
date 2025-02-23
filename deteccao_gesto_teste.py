4d73b31f58ce9930e03061097bf7c28e57cfa48a
Eduardo Augusto Miranda Junior <eduardoaugustomirandajunior@gmail.com>
2025-02-23 17:29:38 -0300

    Update and rename deteccao_gesto.py to deteccao_gesto_teste.py

--- deteccao_gesto_teste.py
+++ deteccao_gesto_teste.py
@@ -14,16 +14,25 @@ while True:
     results = Hand.process(imgRGB) #Imagem Colorida
     handsPoints = results.multi_hand_landmarks #acha os ponto da mao
     h,w,_ = img.shape
+    pontos = [] #array pra guardar cada posição de pontos
     if handsPoints: #Continua a parada mesmo sem ter mão na tela
         for points in handsPoints:
             #print(points)
             mpDraw.draw_landmarks(img, points, hand.HAND_CONNECTIONS)
             for id,cord in enumerate(points.landmark): #pega os ponto e coloca um numer]
                 cx,cy = int(cord.x*w), int(cord.y*h)
-                cv2.putText(img,str(id),(cx,cy+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,0,0), 2)
+                #cv2.putText(img,str(id),(cx,cy+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,0,0), 2)
+                pontos.append((cx,cy)) #coordenada pra cada ponto
+                #print(pontos)
+        dedos = [8,12,16,20] #pontos dos dedos
+        contador= 0
+        if points:
+            for x in dedos:
+                if pontos[x][1] < pontos[x-2][1]: #verifica se o valor da ponta do dedo tá menor que o ponto do inicio do dedo
+                    contador +=1
 
+        print(contador)
 
-                
     cv2.imshow("Imagem",img)
     cv2.waitKey(1)
 
